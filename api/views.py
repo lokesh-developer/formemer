fac1c8a0a10bebcef7099c4cfd47bad061b7e614
aniket811 <ajosh4347@gmail.com>
2022-03-17 21:57:37 +0530

    init

---
+++ api/views.py
@@ -0,0 +1,13 @@
+
+from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import TodoSerializer
+
+from .models import Todo
+# Create your views here.
+
+
+class TodoListView(generics.ListAPIView):
+    model = Todo
+    serializer_class = TodoSerializer
+    
